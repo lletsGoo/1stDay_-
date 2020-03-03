68d04a3904d01528ddeb87e441874fb2345f3d84
lletsGoo <33899717+lletsGoo@users.noreply.github.com>
2020-03-03 16:11:58 +0800

    Delete generateDic.py

--- generateDic.py
+++
@@ -1,9 +0,0 @@
-import itertools as its
-words="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"#如需要，可加入大写字母及其他符号
-dic=open('pwd.txt','w')
-for num in range(1,5):#度为8~10位数# # #
-    keys=its.product(words,repeat=num)
-    for key in keys:
-        dic.write("".join(key) + "\n")
-
-dic.close()
