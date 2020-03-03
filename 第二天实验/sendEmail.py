f09dc59517a05c1276a0471f45ed61c34da78952
lletsGoo <33899717+lletsGoo@users.noreply.github.com>
2020-03-03 16:11:29 +0800

    Add files via upload

---
+++ 第二天实验/sendEmail.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import platform
+
+if __name__ == '__main__':
+    fromaddr = 'zcd1148241614@163.com'
+    password = 'zcd158358'
+    toaddrs = ['1148241614@qq.com', 'zcd1148241614@163.com']
+
+    result = platform.platform()
+    content = result.read()
+    textApart = MIMEText(content)
+    #print(content)
+    zipFile = 'zipnew.zip'
+    zipApart = MIMEApplication(open(zipFile, 'rb').read())
+    zipApart.add_header('Content-Disposition', 'attachment', filename=zipFile)
+
+    m = MIMEMultipart()
+    m.attach(textApart)
+    m.attach(zipApart)
+    m['Subject'] = 'title'
+
+    try:
+        server = smtplib.SMTP('smtp.163.com')
+        server.login(fromaddr, password)
+        server.sendmail(fromaddr, toaddrs, m.as_string())
+        print('success')
+        server.quit()
+    except smtplib.SMTPException as e:
+        print('error:', e)  # 打印错误
